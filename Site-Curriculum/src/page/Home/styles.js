9567b4ea75d19a6478e9ff1c7a5603d0cfaedfc6
murilolemes <murilo.lemes.mhl@gmail.com>
2020-11-05 11:02:37 -0300

    Adjustments CSS responsibily for screens max width 450px

--- Site-Curriculum/src/page/Home/styles.js
+++ Site-Curriculum/src/page/Home/styles.js
@@ -5,10 +5,6 @@ import imgAvatar from '../../assets/foto_avatar.jpg';
 export const Container = styled.div`
   padding: 50px 200px 20px 200px;
 
-  .container {
-    max-width: 800px;
-  }
-
   p {
     color: #32264d;
     font-size: 1.5rem;
@@ -18,6 +14,25 @@ export const Container = styled.div`
     text-decoration: none;
     color: #32264d;
     font-size: 1.5rem;
+  }
+
+  @media (max-width: 450px) {
+    padding: 20px 10px;
+  }
+
+  @media (min-width: 1440px) {
+    .classContent {
+      max-width: 750px;
+    }
+  }
+
+  @media (min-width: 1600px) {
+    margin: 0 auto;
+    max-width: 1600px;
+
+    .classContent {
+      max-width: 1000px;
+    }
   }
 `;
 
@@ -31,32 +46,59 @@ export const Content = styled.div`
     width: 100%;
     max-width: 800px;
   }
+
+  @media (max-width: 450px) {
+    flex-direction: column-reverse;
+  }
 `;
 
 export const Title = styled.div`
-  font-size: 32px;
+  font-size: 3.2rem;
   text-align: center;
   padding-bottom: 25px;
-  box-shadow: 0 1px 0 #e6e6f0;
+  border-bottom: 1px solid #e6e6f0;
+
+  @media (max-width: 400px) {
+    font-size: 1.2rem;
+  }
+
+  @media (max-width: 450px) {
+    font-size: 1.4rem;
+  }
 `;
 
 export const AboutMe = styled.div.attrs({
-  className: 'container',
+  className: 'classContent',
 })`
   padding: 10px 0 25px 0;
-  box-shadow: 0 1px 0 #e6e6f0;
+  border-bottom: 1px solid #e6e6f0;
 
   p {
     margin-top: 10px;
     line-height: 25px;
   }
+
+  @media (max-width: 450px) {
+    border-top: 1px solid #e6e6f0;
+    border-bottom: 1px solid #e6e6f0;
+
+    h2 {
+      font-size: 2.4rem;
+      text-align: center;
+    }
+
+    p {
+      font-size: 1.5rem;
+      text-align: justify;
+    }
+  }
 `;
 
 export const PersonalData = styled.div.attrs({
-  className: 'container',
+  className: 'classContent',
 })`
   padding: 10px 0 25px 0;
-  box-shadow: 0 1px 0 #e6e6f0;
+  border-bottom: 1px solid #e6e6f0;
 
   div {
     display: flex;
@@ -76,46 +118,123 @@ export const PersonalData = styled.div.attrs({
     display: flex;
     align-items: center;
   }
+
+  @media (max-width: 450px) {
+    h2 {
+      font-size: 2.4rem;
+      text-align: center;
+    }
+
+    div {
+      display: block;
+      text-align: center;
+    }
+
+    h4 {
+      font-size: 1.6rem;
+      margin: 0;
+    }
+
+    p {
+      font-size: 1.5rem;
+      text-align: center;
+      width: 100%;
+      display: block;
+      line-height: 25px;
+    }
+
+    a {
+      font-size: 1.5rem;
+      text-align: center;
+      width: 100%;
+      display: block;
+    }
+  }
 `;
 
 export const Formations = styled.div.attrs({
-  className: 'container',
+  className: 'classContent',
 })`
   padding: 10px 0 25px 0;
-  box-shadow: 0 1px 0 #e6e6f0;
+  border-bottom: 1px solid #e6e6f0;
 
   p {
     margin-top: 10px;
+    line-height: 25px;
+  }
+
+  @media (max-width: 450px) {
+    h2 {
+      font-size: 2.4rem;
+      text-align: center;
+    }
+
+    p {
+      font-size: 1.5rem;
+      text-align: center;
+      width: 100%;
+      display: block;
+    }
   }
 `;
 
 export const Improvement = styled.div.attrs({
-  className: 'container',
+  className: 'classContent',
 })`
   padding: 10px 0 25px 0;
-  box-shadow: 0 1px 0 #e6e6f0;
+  border-bottom: 1px solid #e6e6f0;
 
   p {
     margin-top: 10px;
+    line-height: 25px;
+  }
+
+  @media (max-width: 450px) {
+    h2 {
+      font-size: 2.4rem;
+      text-align: center;
+    }
+
+    p {
+      font-size: 1.5rem;
+      text-align: center;
+      width: 100%;
+      display: block;
+    }
   }
 `;
 
 export const Knowledge = styled.div.attrs({
-  className: 'container',
+  className: 'classContent',
 })`
   padding: 10px 0 25px 0;
-  box-shadow: 0 1px 0 #e6e6f0;
+  border-bottom: 1px solid #e6e6f0;
 
   p {
     margin-top: 10px;
+    line-height: 25px;
+  }
+
+  @media (max-width: 450px) {
+    h2 {
+      font-size: 2.4rem;
+      text-align: center;
+    }
+
+    p {
+      font-size: 1.5rem;
+      text-align: center;
+      width: 100%;
+      display: block;
+    }
   }
 `;
 
 export const ProfessionalExperience = styled.div.attrs({
-  className: 'container',
+  className: 'classContent',
 })`
   padding: 10px 0 25px 0;
-  box-shadow: 0 1px 0 #e6e6f0;
+  border-bottom: 1px solid #e6e6f0;
 
   h2 {
     margin-bottom: 10px;
@@ -123,8 +242,7 @@ export const ProfessionalExperience = styled.div.attrs({
 
   div {
     display: flex;
-    margin-top: 5px;
-    margin: 10px 0;
+    margin-top: 15px;
   }
 
   h4 {
@@ -135,16 +253,55 @@ export const ProfessionalExperience = styled.div.attrs({
     display: flex;
     align-items: center;
   }
+
+  @media (max-width: 450px) {
+    h2 {
+      font-size: 2.4rem;
+      text-align: center;
+    }
+
+    p {
+      font-size: 1.5rem;
+      text-align: center;
+      width: 100%;
+      display: block;
+      line-height: 25px;
+    }
+
+    div {
+      display: block;
+      text-align: center;
+    }
+
+    h4 {
+      font-size: 1.6rem;
+      margin: 0;
+    }
+  }
 `;
 
 export const Objective = styled.div.attrs({
-  className: 'container',
+  className: 'classContent',
 })`
   padding: 10px 0 25px 0;
 
   p {
     margin-top: 10px;
     line-height: 25px;
+  }
+
+  @media (max-width: 450px) {
+    h2 {
+      font-size: 2.4rem;
+      text-align: center;
+    }
+
+    p {
+      font-size: 1.5rem;
+      text-align: justify;
+      width: 100%;
+      display: block;
+    }
   }
 `;
 
@@ -157,11 +314,15 @@ export const Avatar = styled.div`
   min-height: 224px;
   border-radius: 50%;
   margin: 10px 0 30px auto;
+
+  @media (max-width: 450px) {
+    margin: 15px auto;
+  }
 `;
 
 export const Footer = styled.footer`
   padding-top: 15px;
-  box-shadow: 0 -1px 0 #e6e6f0;
+  border-top: 1px solid #e6e6f0;
   text-align: center;
 
   #links {
