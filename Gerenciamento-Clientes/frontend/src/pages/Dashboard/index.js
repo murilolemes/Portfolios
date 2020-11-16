550711c20832f74e74063b03c31e0e6c720ce49c
murilolemes <murilo.lemes.mhl@gmail.com>
2020-11-16 17:59:06 -0300

    dashboard tables

--- Gerenciamento-Clientes/frontend/src/pages/Dashboard/index.js
+++ Gerenciamento-Clientes/frontend/src/pages/Dashboard/index.js
@@ -1,60 +1,293 @@
-import React, { useState, useCallback } from 'react';
-import { FiX, FiMenu } from 'react-icons/fi';
+import React from 'react';
+import { FiEdit } from 'react-icons/fi';
 
 import Header from '../../components/Header';
 
-import { Container, Icon, Menu, Content, Body, Title } from './styles';
+import {
+  Container,
+  Content,
+  Body,
+  Title,
+  ActiveClient,
+  InativeCustomers,
+  ServersList,
+  PlansList,
+  DivTable,
+  DivButton,
+} from './styles';
 
-const Dashboard = () => {
-  const [isMenu, setIsMenu] = useState(false);
-  const [isDisplayMenu, setIsDisplayMenu] = useState('displayOff');
-
-  const handleMenu = useCallback(() => {
-    if (isMenu) {
-      setIsMenu(false);
-      setIsDisplayMenu('displayOff');
-    } else {
-      setIsMenu(true);
-      setIsDisplayMenu('displayOn');
-    }
-  }, [isMenu]);
-
-  return (
-    <Container>
-      <Icon onClick={handleMenu}>
-        {!isMenu ? <FiMenu size={34} /> : <FiX size={34} />}
-      </Icon>
-
-      <Menu>
-        <div id={isDisplayMenu}>
-          <Header />
-        </div>
-      </Menu>
-
+const Dashboard = () => (
+  <Container>
+    <Header />
+    <Content>
       <Body>
         <div id="title">
           <Title>Sistema de Gerenciamento de Clientes</Title>
         </div>
         <div className="tables">
-          <div className="activeClients">
-            <h2>Lista de clientes Ativos</h2>
-          </div>
-          <div className="inativeCustomers">
+          <ActiveClient className="activeClients">
+            <h2>Lista de Clientes Ativos</h2>
+            <DivTable>
+              <table>
+                <tr>
+                  <th>Nome</th>
+                  <th>Servidor</th>
+                  <th>Vencimento</th>
+                  <th>Situação</th>
+                  <th>Ver</th>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Pago</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Pago</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Pago</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Pago</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Pago</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Pago</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+              </table>
+            </DivTable>
+            <DivButton className="divButton">
+              <button type="button">Ver Todos</button>
+            </DivButton>
+          </ActiveClient>
+          <InativeCustomers className="inativeCustomers">
             <h2>Lista de clientes Inativos</h2>
-          </div>
+            <DivTable>
+              <table>
+                <tr>
+                  <th>Nome</th>
+                  <th>Servidor</th>
+                  <th>Vencimento</th>
+                  <th>Situação</th>
+                  <th>Ver</th>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Deve</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Deve</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Deve</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Deve</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Deve</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Murilo Henrique Lemes</td>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>Deve</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+              </table>
+            </DivTable>
+            <DivButton className="divButton">
+              <button type="button">Ver Todos</button>
+            </DivButton>
+          </InativeCustomers>
         </div>
         <div className="tables">
-          <div className="serversList">
+          <ServersList className="serversList">
             <h2>Lista de Servidores</h2>
-          </div>
-          <div className="plansList">
+            <DivTable>
+              <table>
+                <tr>
+                  <th>Nome</th>
+                  <th>Vencimento</th>
+                  <th>Creditos</th>
+                  <th>Ver</th>
+                </tr>
+                <tr>
+                  <td>Blade</td>
+                  <td>16/11/2020</td>
+                  <td>300</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+              </table>
+            </DivTable>
+            <DivButton className="divButton">
+              <button type="button">Ver Todos</button>
+            </DivButton>
+          </ServersList>
+          <PlansList className="plansList">
             <h2>Lista de Planos</h2>
-          </div>
+            <DivTable>
+              <table>
+                <tr>
+                  <th>Nome</th>
+                  <th>Servidor</th>
+                  <th>Status</th>
+                  <th>Ver</th>
+                </tr>
+                <tr>
+                  <td>Plano Anual</td>
+                  <td>Blade</td>
+                  <td>Ativo</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Plano Mensal</td>
+                  <td>Blade</td>
+                  <td>Ativo</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Plano Semanal</td>
+                  <td>Blade</td>
+                  <td>Ativo</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Plano Di[ario</td>
+                  <td>Blade</td>
+                  <td>Ativo</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Plano anual</td>
+                  <td>Blade</td>
+                  <td>Ativo</td>
+                  <td>
+                    <button type="button">
+                      <FiEdit />
+                    </button>
+                  </td>
+                </tr>
+              </table>
+            </DivTable>
+            <DivButton className="divButton">
+              <button type="button">Ver Todos</button>
+            </DivButton>
+          </PlansList>
         </div>
       </Body>
-
-      <Content />
-    </Container>
-  );
-};
+    </Content>
+  </Container>
+);
 export default Dashboard;
--- Gerenciamento-Clientes/frontend/src/pages/Dashboard/styles.js
+++ Gerenciamento-Clientes/frontend/src/pages/Dashboard/styles.js
@@ -6,37 +6,13 @@ export const Container = styled.div`
   display: flex;
 `;
 
-export const Icon = styled.button`
-  background: transparent;
-  border: 0;
-  position: absolute;
-  margin-top: 25px;
-  margin-left: 15px;
-  color: #fafafa;
-
-  &:hover {
-    color: ${shade(0.2, '#ff9000')};
-  }
-`;
-
-export const Menu = styled.div`
-  #displayOff {
-    display: none;
-  }
-
-  #displayOn {
-    height: 100%;
-    width: 350px;
-    box-shadow: 0 -5px 5px ${shade(0.2, '#ff9000')};
-  }
-`;
-
 export const Content = styled.div`
   flex: 1;
 `;
 
 export const Body = styled.div`
   width: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   text-align: center;
@@ -45,12 +21,6 @@ export const Body = styled.div`
   #title {
     display: flex;
     align-items: flex-start;
-
-    button {
-      background: transparent;
-      border: 0;
-      color: #fafafa;
-    }
   }
 
   .tables {
@@ -67,6 +37,12 @@ export const Body = styled.div`
 
       & + div {
         margin-left: 10px;
+      }
+
+      .divButton {
+        box-shadow: none;
+        margin-left: 0;
+        text-align: right;
       }
     }
 
@@ -86,10 +62,137 @@ export const Body = styled.div`
       flex: 1;
     }
   }
+
+  @media (max-width: 720px) {
+    display: flex;
+    align-items: center;
+
+    #title {
+      width: 300px;
+
+      h1 {
+        font-size: 2.5rem;
+      }
+    }
+
+    .tables {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+
+      & + .tables {
+        margin-top: 13px;
+      }
+
+      div {
+        padding: 10px;
+        box-shadow: 0 0 5px ${shade(0.2, '#ff9000')};
+
+        & + div {
+          margin: 0;
+          margin-top: 13px;
+        }
+      }
+
+      .activeClients {
+        flex: 1;
+      }
+
+      .inativeCustomers {
+        flex: 1;
+      }
+
+      .serversList {
+        flex: 1;
+      }
+
+      .plansList {
+        flex: 1;
+      }
+    }
+  }
 `;
 
 export const Title = styled.h1`
   flex: 1;
   margin-bottom: 20px;
   text-shadow: 0 2px 2px ${shade(0.2, '#ff9000')};
+`;
+
+export const ActiveClient = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InativeCustomers = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ServersList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PlansList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const DivTable = styled.div`
+  flex: 1;
+  box-shadow: none !important;
+
+  table {
+    border-collapse: collapse;
+    width: 100%;
+
+    th,
+    tr,
+    td {
+      box-shadow: 0 0 1px ${shade(0.2, '#ff9000')};
+    }
+
+    th {
+      height: 40px;
+      color: ${shade(0.2, '#ff9000')};
+    }
+
+    tr {
+      td {
+        height: 30px;
+
+        button {
+          border: 0;
+          background: transparent;
+          color: #fafafa;
+        }
+
+        svg {
+          width: 1.4rem;
+          height: 1.8rem;
+        }
+      }
+
+      &:hover {
+        color: ${shade(0.2, '#ff9000')};
+
+        button {
+          color: ${shade(0.2, '#ff9000')};
+        }
+      }
+    }
+  }
+`;
+
+export const DivButton = styled.div`
+  button {
+    border: 0;
+    background: transparent;
+    color: #fafafa;
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
 `;
