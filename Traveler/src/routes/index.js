9937c56f7c396e04d44b986765a156817f89bf11
murilolemes <murilo.lemes.mhl@gmail.com>
2021-02-03 17:17:32 -0300

    adding city list route

--- Traveler/src/routes/index.js
+++ Traveler/src/routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from '../pages/home';
+import Home from '../pages/Home';
+import CityList from '../pages/CityList';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/cityList" exact component={CityList} />
   </Switch>
 );
 
